754859fd801bdf999a11a8f47b5e9bef693b125d
Yaswanth <yaswanthkatta97@gmail.com>
2025-03-27 16:33:47 +0530

    datatypes

---
+++ WebTechnologies/26_03_20225/dataType.js
@@ -0,0 +1,13 @@
+//var
+var a="Yaswanth"
+console.log(a);
+
+//let
+let b="23"
+console.log(b);
+
+//const 
+const c='male'
+console.log(c);
+
+
---
+++ WebTechnologies/27_03_2025/DataTypes.js
@@ -0,0 +1,35 @@
+//DATA TYPES
+//primitive datatypes
+
+//string
+var userName="Yaswanth"
+console.log(userName);
+
+//number
+let age=23
+console.log(age);
+
+//boolean
+const bool=true
+console.log(bool);
+
+//undefined
+var a
+console.log(a);
+
+//null
+let b=null
+console.log(b);
+
+//type of operator
+console.log(typeof userName);
+console.log(typeof age);
+console.log(typeof bool);
+console.log(typeof a);
+console.log(typeof b);
+
+
+
+
+
+
