f1fee2332a4d04e6a65c96b2a620453cc9425866
Yaswanth <yaswanthkatta97@gmail.com>
2025-03-28 17:55:44 +0530

    arrays

--- WebTechnologies/28_03_2025/reference.js
+++ WebTechnologies/28_03_2025/reference.js
@@ -1,13 +1,14 @@
 //Arrays
 //Homogeneous
-
-var bikes=["GT","RoyalEnfiled","KTM","Duke","Pulsar"]
+var honda="Hero Honda"
+var bikes=["GT","RoyalEnfiled","KTM","Duke","Pulsar",honda]
 console.log(bikes);
 console.log(bikes.length);
 console.log(bikes[3]);
 
 //Heterogenous
-var fav=[100,'Yaswanth','Dindi',true,null,undefined]
+var food='biryani'
+var fav=[100,'Yaswanth','Dindi',true,null,undefined,[10,20,30,40],food]
 console.log(fav);
 console.log(fav.length);
 console.log(fav[5]);
