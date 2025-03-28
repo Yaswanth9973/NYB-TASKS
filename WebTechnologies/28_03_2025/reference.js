47ee44a4685b4e338e3ce5909f1153559f4da619
Yaswanth <yaswanthkatta97@gmail.com>
2025-03-28 16:55:38 +0530

    reference datatypes

---
+++ WebTechnologies/28_03_2025/reference.js
@@ -0,0 +1,27 @@
+//Arrays
+//Homogeneous
+
+var bikes=["GT","RoyalEnfiled","KTM","Duke","Pulsar"]
+console.log(bikes);
+console.log(bikes.length);
+console.log(bikes[3]);
+
+//Heterogenous
+var fav=[100,'Yaswanth','Dindi',true,null,undefined]
+console.log(fav);
+console.log(fav.length);
+console.log(fav[5]);
+
+//Objects   
+var empDetails={
+    firstName:"Yaswanth",
+    lastName:"Katta",
+    MobileNo:9876543210,
+    Email:"yaswanthkatta@gmail.com",
+    address:"Dindi,EastGodavari,AP" 
+}
+console.log(empDetails);
+console.log(empDetails.Email);
+console.log(empDetails["lastName"]);
+
+
